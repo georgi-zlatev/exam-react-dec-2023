1282bdefd48e7a0c29cce0b23f8b9688870f3f79
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2023-12-03 18:26:57 +0200

    Updated register, when register, logged in automatically

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,25 @@
 import './Register.css'
 
+import { useContext } from "react";
+import AuthContext from "../../contexts/authContexts";
+import useForm from "../../hooks/useForm";
+
+const RegisterFormKeys = {
+  Email: "email",
+  Password: "password",
+  ConfirmPassword: "confirm-password",
+};
+
 export default function Register() {
+  const { registerSubmitHandler } = useContext(AuthContext);
+  const {values, onChange, onSubmit} = useForm(registerSubmitHandler, {
+    [RegisterFormKeys.Email]: '',
+    [RegisterFormKeys.Password]: '',
+    [RegisterFormKeys.ConfirmPassword]: '',
+  });
     return(
 <section id="register-page" className="content auth">
-  <form id="register">
+  <form id="register"  onSubmit={onSubmit}>
     <div className="container">
       <div className="brand-logo"></div>
       <h1>Register</h1>
@@ -14,15 +30,19 @@ export default function Register() {
         id="email"
         name="email"
         placeholder="example@gmail.com"
+        onChange={onChange}
+        values={values[RegisterFormKeys.Email]}
       />
 
       <label htmlFor="pass">Password:</label>
-      <input type="password" name="password" id="register-password" />
+      <input type="password" name="password" id="register-password"  onChange={onChange}
+            values={values[RegisterFormKeys.Password]}/>
 
       <label htmlFor="con-pass">Confirm Password:</label>
-      <input type="password" name="confirm-password" id="confirm-password" />
+      <input type="password" name="confirm-password" id="confirm-password"  onChange={onChange}
+            values={values[RegisterFormKeys.ConfirmPassword]}/>
 
-      <input className="btn submit" type="submit" value="Register" />
+      <input className="btn submit" type="submit" value="Register"  />
 
       <p className="field">
         <span>If you already have profile click <a href="/login">here</a></span>
