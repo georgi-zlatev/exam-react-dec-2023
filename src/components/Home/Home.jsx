8293536a9b2fc0f3879275c9a76a3900a99bf158
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2023-12-07 22:10:03 +0200

    Restored a static home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from "react";
-import * as locationService from '../../services/locationService';
+import React from "react";
+
 import "./Home.css"; // Import the CSS file
 import { Link } from "react-router-dom";
-import LatestLocation from "./LatestGame/LatestGame";
-import withAuth from "../../HOC/withAuth";
 
-function Home({
-  _id,
-  accessToken,
-  email,
-}) {
-  const [latestLocations, setLatestLocations] = useState([])
-
-  useEffect(() => {
-    locationService.getLatest()
-    .then(result => setLatestLocations(result))
-  }, [])
-
-
+export default function Home() {
   return (
     <div className="welcome-world">
       <h1>Welcome to Local Food Events</h1>
@@ -27,17 +13,19 @@ function Home({
         eateries to hidden gems, Local Food Events provides a curated guide to
         satisfy your cravings.
       </p>
-<br />
-<h1>Latest Locations</h1>
-{latestLocations.map(location => <LatestLocation {...location} />)}
 
-{!latestLocations.length && <p>There are no locations currently!</p>}
-     
+
+
+      <div className="card">
+  <div className="card-body">
+  <img src='/recipenoodlespastaicon_109880.ico' className="card-img-top" alt={location.title}/>
+    <h5 className="card-title">  Discover nice brunch and lunch places in Sofia 
+</h5>
+    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <Link to={`/locations`} className="btn btn-primary">All Locations</Link>
+  </div>
+</div>
 
     </div>
   );
 }
-
-const EnchancedHome = withAuth(Home);
-
-export default EnchancedHome;
